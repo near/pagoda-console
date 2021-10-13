6a471f53c02a6f02928ae280a49aa0eb0c3b9f12
Michael Peter <m.peter.dev@gmail.com>
2021-10-13 14:22:25 +0000

    enable cors. change root response to JSON

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // TODO re-evaluate need for CORS once we have domains
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
