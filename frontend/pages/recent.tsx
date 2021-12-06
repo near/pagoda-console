21070b51ddf9fd4dd967bb9de97400eb95c3d453
Michael Peter <m.peter.dev@gmail.com>
2021-12-06 19:38:59 +0000

    remove temporary development page for recent transactions

--- frontend/pages/recent.tsx
+++
@@ -1,8 +0,0 @@
-import RecentTransactionList from "../components/RecentTransactionList";
-import { useSimpleLayout } from "../utils/layouts";
-
-export default function Recent() {
-    return <RecentTransactionList contracts={['test.testnet']} net='TESTNET' />
-}
-
-Recent.getLayout = useSimpleLayout;
