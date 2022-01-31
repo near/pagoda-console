076b88d1a49d94a55b4e75f58410c346cce3d678
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-01-31 03:23:31 +0000

    create an mdx page for tutorial

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,16 @@
 const { i18n } = require('./next-i18next.config');
 
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
@@ -13,4 +22,4 @@ module.exports = {
   },
   i18n,
   swcMinify: true
-}
+});
--- frontend/pages/nft-market-tutorial.tsx
+++
@@ -1,9 +0,0 @@
-import { useDashboardLayout } from "../utils/layouts"
-
-export default function NftMarketTutorial() {
-    return <div>
-        add tutorial here
-    </div>
-}
-
-NftMarketTutorial.getLayout = useDashboardLayout;
