6c0805e1a8aa2700be84f60843c940e5b0f8cf02
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-02-01 20:56:26 +0000

    next button on tutorial page

--- frontend/pages/tutorials/nfts/overview.tsx
+++ frontend/pages/tutorials/nfts/overview.tsx
@@ -1,4 +1,5 @@
 import ProjectSelector from "../../../components/ProjectSelector";
+import NextStepButton from "../../../components/tutorials/NextStepButton";
 import { useDashboardLayout } from "../../../utils/layouts";
 import Content from './overview.mdx';
 
@@ -6,6 +7,16 @@ export default function Overview() {
     return <>
         <ProjectSelector />
         <Content></Content>
+        <div className="footerContainer"><NextStepButton path="/tutorials/nfts/pre-deployed-contract" label="Step-1: Pre-deployed Contract" /></div>
+        <style jsx>{`
+            .footerContainer {
+                margin-top: 1.25rem;
+                display: flex;
+                flex-direction: row;
+                justify-content: flex-end;
+                align-items: center;
+            }
+        `}</style>
     </>;
 }
 
