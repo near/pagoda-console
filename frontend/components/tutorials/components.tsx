12ab95d6b8c1b0dc3cbe3296e51929a592381b0d
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-02-02 05:07:20 +0000

    fix component mapping

--- frontend/components/tutorials/components.tsx
+++ frontend/components/tutorials/components.tsx
@@ -4,7 +4,7 @@ import H2Anchor from "./H2Anchor";
 // This allows customizing the markdown output while the leaving customizations outside of markdown itself.
 // For more info: https://mdxjs.com/table-of-components/
 const components = {
-    h2: ({ children }: { children: any }) => <H2Anchor>{children}</H2Anchor>
+    h2: H2Anchor
 };
 
 export default components;
