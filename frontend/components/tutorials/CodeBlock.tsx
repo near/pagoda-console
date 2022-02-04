1a01a04299a3a583f0a2f021d026cb5caccb26d3
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-02-04 18:44:33 +0000

    strip the newline if one exists

--- frontend/components/tutorials/CodeBlock.tsx
+++ frontend/components/tutorials/CodeBlock.tsx
@@ -5,7 +5,13 @@ export default function CodeBlock(props: any) {
     // It's currently the only way for us to guess if the writer is using a single tick (`) vs three ticks (```).
     // Another way around this would be to call the component directly in Markdown.
     if (props.className) {
-        return <OtherCodeBlock>{props.children}</OtherCodeBlock>;
+        let content;
+        if (typeof props.children === 'string' && props.children.endsWith('\n')) {
+            content = props.children.slice(0, props.children.length - 1);
+        } else {
+            content = props.children;
+        }
+        return <OtherCodeBlock>{content}</OtherCodeBlock>;
     }
     return <code>{props.children}</code>;
 }
