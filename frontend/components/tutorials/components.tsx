b910c44c331e5851b43b53923579c67e5f91471e
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-02-08 01:08:08 +0000

    adding blockquote styling

--- frontend/components/tutorials/components.tsx
+++ frontend/components/tutorials/components.tsx
@@ -2,6 +2,7 @@ import { Table } from "react-bootstrap";
 import CodeBlock from "./CodeBlock";
 import { H1Anchor, H2Anchor, H3Anchor } from "./Anchors";
 import Note from "./Note";
+import Blockquote from "./Blockquote";
 
 // A component map between markdown syntax and custom JSX components.
 // This allows customizing the markdown output while the leaving customizations outside of markdown itself.
@@ -27,7 +28,8 @@ const components = {
     Note: Note,
     Table: (props: any) => {
         return <Table striped bordered hover responsive style={{ marginBottom: '1rem', backgroundColor: 'var(--color-white)' }}>{props.children}</Table>
-    }
+    },
+    blockquote: Blockquote
 };
 
 export default components;
