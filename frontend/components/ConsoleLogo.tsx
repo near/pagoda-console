517f6f5bc2a3264272596aadfc8bf6803b2302c9
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-02-10 20:42:37 +0000

    remove width from logo

--- frontend/components/ConsoleLogo.tsx
+++ frontend/components/ConsoleLogo.tsx
@@ -6,10 +6,7 @@ function ConsoleLogo() {
         <ConsoleLogoImage style={{ width: '100%', height: '100%' }} />
         <style jsx>{`
         .logoContainer {
-            width : 36.5rem;
             max-width: 100vw;
-            // TODO update the svg so it doesn't take up whitespace on the right
-            margin-right: -3.5rem;
         }
         `}</style>
     </div>
--- frontend/components/SmallConsoleLogo.tsx
+++ frontend/components/SmallConsoleLogo.tsx
@@ -6,7 +6,6 @@ function SmallConsoleLogo() {
         <ConsoleLogoImage style={{ width: '100%', height: '100%' }} />
         <style jsx>{`
         .logoContainer {
-            width : 36.5rem;
             max-width: 100vw;
         }
         `}</style>
