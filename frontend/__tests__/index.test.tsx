7cb014543df3e231818cfee2618997a30182aa62
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-03-01 10:59:44 -0700

    lock all packages, add jest

--- frontend/__tests__/index.test.tsx
+++ frontend/__tests__/index.test.tsx
@@ -1,15 +1,19 @@
 import { render, screen } from '@testing-library/react'
 import Login from '../pages/index'
 
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: () => {
+    return 'Next image stub'; // whatever
+  },
+}));
+
 describe('Login', () => {
-    it('renders a login page', () => {
-        render(<Login />)
+  it('renders a login page', () => {
+    render(<Login />)
 
-        const terms = screen.getByText('Terms of Use');
-        expect(terms).toBeInTheDocument();
+    const terms = screen.getByText('Terms of Use');
+    expect(terms).toBeInTheDocument();
 
-        const privacy = screen.getByText('Privacy Policy');
-        expect(privacy).toBeInTheDocument();
-
-    });
+  });
 });
