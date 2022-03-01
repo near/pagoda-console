e13411b226fac3a20519cffa411ec29c65a82ef1
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-03-01 18:10:44 +0000

    move mock out of test

--- frontend/__tests__/index.test.tsx
+++ frontend/__tests__/index.test.tsx
@@ -1,19 +1,12 @@
 import { render, screen } from '@testing-library/react'
 import Login from '../pages/index'
 
-jest.mock('next/image', () => ({
-  __esModule: true,
-  default: () => {
-    return 'Next image stub'; // whatever
-  },
-}));
-
 describe('Login', () => {
-  it('renders a login page', () => {
-    render(<Login />)
+    it('renders a login page', () => {
+        render(<Login />)
 
-    const terms = screen.getByText('Terms of Use');
-    expect(terms).toBeInTheDocument();
+        const terms = screen.getByText('Terms of Use');
+        expect(terms).toBeInTheDocument();
 
-  });
+    });
 });
