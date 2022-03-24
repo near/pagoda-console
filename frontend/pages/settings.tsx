951163108db982c6744c69b3bea099006bee2718
Caleb Jacob <thecalebjacob@gmail.com>
2022-03-24 11:17:37 -0600

    Added min-height rule to avoid edit shifting

--- frontend/pages/settings.tsx
+++ frontend/pages/settings.tsx
@@ -147,6 +147,7 @@ export default function Settings() {
           flex-direction: row;
           justify-content: space-between;
           align-items: center;
+          min-height: 2.5rem;
         }
         .settingLabel {
           font-weight: 600;
