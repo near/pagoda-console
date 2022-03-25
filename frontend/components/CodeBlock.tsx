68b54308410de3bf6e9988092d0cb7e1e609506f
Caleb Jacob <thecalebjacob@gmail.com>
2022-03-25 13:50:13 -0600

    Converting CodeBlock away from FC to be consistent with other components

--- frontend/components/CodeBlock.tsx
+++ frontend/components/CodeBlock.tsx
@@ -1,11 +1,11 @@
 import { faCopy } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { FC, useRef, useState } from 'react';
+import { ReactElement, useRef, useState } from 'react';
 import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { LightAsync as SyntaxHighlighter, SyntaxHighlighterProps } from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
-const CodeBlock: FC<SyntaxHighlighterProps> = function ({ children, ...passedProps }) {
+export default function CodeBlock({ children, ...passedProps }: { children: ReactElement; language: string }) {
   const isChildString = typeof children === 'string';
 
   let [showCopiedAlert, setShowCopiedAlert] = useState(false);
@@ -64,6 +64,4 @@ const CodeBlock: FC<SyntaxHighlighterProps> = function ({ children, ...passedPro
       `}</style>
     </div>
   );
-};
-
-export default CodeBlock;
+}
