741d8c536f6aff670c33ecfa49fa8f50ad0b12fa
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-05-19 13:23:32 -0600

    switch to regexp

--- frontend/tests/playwright/fixtures.ts
+++ frontend/tests/playwright/fixtures.ts
@@ -3,7 +3,7 @@ import { test as base } from '@playwright/test';
 export const test = base.extend({
   page: async ({ page }, use) => {
     // This will block all requests to fullstory so our testing won't create user sessions there.
-    await page.route('*.fullstory.com/**', (route) => route.abort());
+    await page.route(/(\.fullstory\.com\/)/, (route) => route.abort());
     await use(page);
   },
 });
