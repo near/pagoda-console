108cb0eaeb61dd41b5d4cd41f47e4c92e2556001
Caleb Jacob <thecalebjacob@gmail.com>
2022-06-16 15:19:37 -0600

    Copy webhook header secret value

--- frontend/modules/alerts/components/WebhookDestinationSecret.tsx
+++ frontend/modules/alerts/components/WebhookDestinationSecret.tsx
@@ -1,6 +1,11 @@
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
+
+import { Button } from '@/components/lib/Button';
 import { Card } from '@/components/lib/Card';
+import { FeatherIcon } from '@/components/lib/FeatherIcon';
 import { Flex } from '@/components/lib/Flex';
 import { Text } from '@/components/lib/Text';
+import { openToast } from '@/components/lib/Toast';
 
 import type { Destination } from '../utils/types';
 
@@ -9,6 +14,15 @@ interface Props {
 }
 
 export function WebhookDestinationSecret({ destination }: Props) {
+  function copySecret() {
+    navigator.clipboard.writeText(`Bearer ${destination.secret}`);
+
+    openToast({
+      type: 'success',
+      title: 'Secret copied to clipboard.',
+    });
+  }
+
   return (
     <Flex stack>
       <Text>
@@ -22,6 +36,10 @@ export function WebhookDestinationSecret({ destination }: Props) {
           <Text family="code" color="text1" weight="semibold">
             Bearer {destination.secret}
           </Text>
+          <Button size="s" color="transparent" onClick={copySecret} css={{ marginLeft: 'auto' }}>
+            <FeatherIcon icon="copy" size="xs" />
+            <VisuallyHidden>Copy Secret</VisuallyHidden>
+          </Button>
         </Flex>
       </Card>
     </Flex>
