eb13b2591cda7fa8317ab521dad02df1571b17d2
Caleb Jacob <thecalebjacob@gmail.com>
2022-06-22 14:11:53 -0600

    Confirm affected alerts when deleting a destination

--- frontend/modules/alerts/components/DeleteDestinationModal.tsx
+++ frontend/modules/alerts/components/DeleteDestinationModal.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+import { Card } from '@/components/lib/Card';
+import { FeatherIcon } from '@/components/lib/FeatherIcon';
+import { Flex } from '@/components/lib/Flex';
+import { H5 } from '@/components/lib/Heading';
+import { List, ListItem } from '@/components/lib/List';
 import { Text } from '@/components/lib/Text';
 import { ConfirmModal } from '@/components/modals/ConfirmModal';
+import { useSelectedProject } from '@/hooks/selected-project';
 
+import { useAlerts } from '../hooks/alerts';
 import { deleteDestination } from '../hooks/destinations';
-import type { Destination } from '../utils/types';
+import type { Alert, Destination } from '../utils/types';
 
 interface Props {
   destination: Destination;
@@ -16,6 +23,18 @@ interface Props {
 export function DeleteDestinationModal({ destination, show, setShow, onDelete }: Props) {
   const [errorText, setErrorText] = useState<string | undefined>();
   const [isDeleting, setIsDeleting] = useState(false);
+  const { environment, project } = useSelectedProject();
+  const { alerts } = useAlerts(project?.slug, environment?.subId);
+  const [enabledAlerts, setEnabledAlerts] = useState<Alert[]>([]);
+
+  useEffect(() => {
+    const result = alerts?.filter((alert) => {
+      const enabledDestination = alert.enabledDestinations.find((d) => d.id === destination.id);
+      return !!enabledDestination;
+    });
+
+    setEnabledAlerts(result || []);
+  }, [alerts, destination]);
 
   async function onConfirm() {
     setIsDeleting(true);
@@ -44,10 +63,46 @@ export function DeleteDestinationModal({ destination, show, setShow, onDelete }:
       show={show}
       title={`Delete Destination`}
     >
-      <Text>This action cannot be undone. Are you sure you want to delete the following destination?</Text>
-      <Text color="text1" weight="semibold">
-        {destination.name}
-      </Text>
+      <Flex stack>
+        <Text>
+          This action cannot be undone. Are you sure you want to delete{' '}
+          <Text color="text1" weight="semibold" as="span">
+            {destination.name}
+          </Text>
+          ?
+        </Text>
+      </Flex>
+
+      <Card padding="m" borderRadius="m">
+        {enabledAlerts.length === 0 ? (
+          <Flex stack>
+            <Flex align="center">
+              <FeatherIcon color="success" icon="check-circle" />
+              <H5>No Affected Alerts</H5>
+            </Flex>
+            <Text>This destination isn&apos;t enabled on any alerts.</Text>
+          </Flex>
+        ) : (
+          <Flex stack>
+            <Flex align="center">
+              <FeatherIcon color="danger" icon="alert-triangle" />
+              <H5>Affected Alerts</H5>
+            </Flex>
+            <Text>This destination will be removed from the following alerts:</Text>
+            <List>
+              {enabledAlerts.map((alert) => {
+                return (
+                  <ListItem key={alert.id}>
+                    <Text color="text1" weight="semibold">
+                      {alert.name}
+                    </Text>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Flex>
+        )}
+      </Card>
     </ConfirmModal>
   );
 }
