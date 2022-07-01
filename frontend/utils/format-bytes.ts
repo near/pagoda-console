2caf54ab8829b2ec0bba3ae9b3fbb1083b4a2e8e
Caleb Jacob <thecalebjacob@gmail.com>
2022-07-01 12:28:22 -0600

    Changed param name to "bytes"

--- frontend/utils/format-bytes.ts
+++ frontend/utils/format-bytes.ts
@@ -1,15 +1,15 @@
 import { truncateDecimal } from './truncate-decimal';
 
-function divide(count: number, divisor: number) {
-  return truncateDecimal(count / divisor, 2);
+function divide(bytes: number, divisor: number) {
+  return truncateDecimal(bytes / divisor, 2);
 }
 
-export function formatBytes(count?: number | unknown) {
-  if (typeof count !== 'number') return '';
+export function formatBytes(bytes?: number | unknown) {
+  if (typeof bytes !== 'number') return '';
 
-  if (count >= 1_000_000_000) return `${divide(count, 1_000_000_000)} GB`;
-  if (count >= 1_000_000) return `${divide(count, 1_000_000)} MB`;
-  if (count >= 1_000) return `${divide(count, 1_000)} KB`;
+  if (bytes >= 1_000_000_000) return `${divide(bytes, 1_000_000_000)} GB`;
+  if (bytes >= 1_000_000) return `${divide(bytes, 1_000_000)} MB`;
+  if (bytes >= 1_000) return `${divide(bytes, 1_000)} KB`;
 
-  return `${count} B`;
+  return `${bytes} B`;
 }
