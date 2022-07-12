9a8ad5bf7c735f88c3f0fb67b8960985e795709a
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-07-12 13:08:01 -0600

    attempt to snapshot snippets only, doesn't quite work correctly

--- frontend/tests/playwright/snapshot/nft-tutorial-snapshot.spec.ts
+++ frontend/tests/playwright/snapshot/nft-tutorial-snapshot.spec.ts
@@ -9,10 +9,13 @@ import { login } from '../login';
   'predeployed-contract',
   'skeleton',
   'upgrade-contract',
-  'enumeration',
-  'core',
-  'royalty',
-  'marketplace',
+  // 'enumeration',
+  // 'core',
+  // 'royalty',
+  // 'marketplace',
+  // 'minting',
+  // 'approvals',
+  // 'events',
 ].forEach((path) => {
   test(`NFT tutorial ${path} snapshot test`, async ({ page }) => {
     const project = process.env.TEST_NFT_TUTORIAL_PROJECT;
@@ -25,45 +28,46 @@ import { login } from '../login';
       waitUntil: 'networkidle',
     });
 
-    expect(
-      await page.screenshot({
-        fullPage: true,
-        mask: [page.locator('.animateOpen')],
-      }),
-    ).toMatchSnapshot(`nft_tutorial_page_${path}.png`);
+    const codeSnippets = await page.locator('pre', {
+      has: page.locator('div a:has-text("See full example on Github")'),
+    });
+    const count = await codeSnippets.count();
+    for (let i = 0; i < count; ++i) {
+      expect(await codeSnippets.nth(i).screenshot()).toMatchSnapshot(`nft_tutorial_page_${path}_${i}.png`);
+    }
   });
 });
 
 // These pages are extremely long and so we will breakup the screenshots in X px chunks because there are loading problems trying to screenshot the whole page at once.
 // Note: `chunks` represent the # of X px chunks it would take to render the whole page.
-const CHUNK = 10000;
-[
-  { path: 'minting', chunks: 2 },
-  { path: 'approvals', chunks: 3 },
-  { path: 'events', chunks: 2 },
-].forEach(({ path, chunks }) => {
-  for (let i = 0; i < chunks; i++) {
-    const from = i * CHUNK;
-    const to = from + CHUNK;
-    test(`NFT tutorial ${path} snapshot - ${from}-${to}k px - test`, async ({ page }) => {
-      const project = process.env.TEST_NFT_TUTORIAL_PROJECT;
+// const CHUNK = 10000;
+// [
+//   { path: 'minting', chunks: 2 },
+//   { path: 'approvals', chunks: 3 },
+//   { path: 'events', chunks: 2 },
+// ].forEach(({ path, chunks }) => {
+//   for (let i = 0; i < chunks; i++) {
+//     const from = i * CHUNK;
+//     const to = from + CHUNK;
+//     test(`NFT tutorial ${path} snapshot - ${from}-${to}k px - test`, async ({ page }) => {
+//       const project = process.env.TEST_NFT_TUTORIAL_PROJECT;
 
-      setupErrorLogger(page);
+//       setupErrorLogger(page);
 
-      // sign in
-      await login(page);
+//       // sign in
+//       await login(page);
 
-      await page.goto(`/tutorials/nfts/${path}?project=${project}&environment=1`, {
-        waitUntil: 'networkidle',
-      });
+//       await page.goto(`/tutorials/nfts/${path}?project=${project}&environment=1`, {
+//         waitUntil: 'networkidle',
+//       });
 
-      expect(
-        await page.screenshot({
-          fullPage: true,
-          mask: [page.locator('.animateOpen')],
-          clip: { height: CHUNK, width: 1280, x: 0, y: from },
-        }),
-      ).toMatchSnapshot(`nft_tutorial_page_${path}_${i}.png`);
-    });
-  }
-});
+//       expect(
+//         await page.screenshot({
+//           fullPage: true,
+//           mask: [page.locator(':above(section)'), page.locator(':left-of(section)'), page.locator(':below(section)')],
+//           clip: { height: CHUNK, width: 1280, x: 0, y: from },
+//         }),
+//       ).toMatchSnapshot(`nft_tutorial_page_${path}_${i}.png`);
+//     });
+//   }
+// });
