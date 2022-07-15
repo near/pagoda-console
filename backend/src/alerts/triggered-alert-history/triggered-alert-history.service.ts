d04b4d6bf2119d5aea9aecda788d288b5d3e34c2
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-07-15 17:46:15 +0000

    update types

--- backend/src/alerts/triggered-alert-history/triggered-alert-history.service.ts
+++ backend/src/alerts/triggered-alert-history/triggered-alert-history.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { Alert, TriggeredAlert } from 'generated/prisma/alerts';
+import { Alert, Prisma, TriggeredAlert } from 'generated/prisma/alerts';
 import { PrismaService } from '../prisma.service';
 import { PermissionsService as ProjectPermissionsService } from 'src/projects/permissions.service';
 import { AlertsService } from '../alerts.service';
@@ -86,8 +86,8 @@ export class TriggeredAlertHistoryService {
     projectSlug: string,
     environmentSubId: number,
     alertId: number,
-  ) {
-    const listWhere: Record<string, any> = {
+  ): Prisma.TriggeredAlertWhereInput {
+    const listWhere: Prisma.TriggeredAlertWhereInput = {
       alert: {
         projectSlug,
         environmentSubId,
@@ -113,8 +113,7 @@ export class TriggeredAlertHistoryService {
       triggeredInReceiptId,
       triggeredAt,
     } = triggeredAlert;
-    const extraData: Record<string, unknown> =
-      triggeredAlert.extraData as Record<string, unknown>;
+    const extraData = triggeredAlert.extraData as Record<string, unknown>;
     const rule = alert.matchingRule as object as MatchingRule;
 
     return {
