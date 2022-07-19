f6caac29cfad6a81d014e30ec3c4c9376d30f058
Caleb Jacob <thecalebjacob@gmail.com>
2022-07-19 15:25:31 -0600

    Fixing truncate middle logic

--- frontend/utils/truncate-middle.ts
+++ frontend/utils/truncate-middle.ts
@@ -4,9 +4,7 @@ export function truncateMiddle(value: string, prefixLength = 4, suffixLength = 4
   const totalLength = prefixLength + suffixLength;
 
   if (value.length > totalLength) {
-    return (
-      value.substring(0, prefixLength) + '...' + value.substring(value.length - (suffixLength + 1), value.length - 1)
-    );
+    return value.slice(0, prefixLength) + '...' + value.slice(suffixLength * -1);
   }
 
   return value;
