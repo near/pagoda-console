83b5ef1bddfc77a0e5b3427d7b5e7f0ad79369c7
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-07-22 06:00:20 +0000

    squash db migrations

--- backend/src/alerts/prisma/migrations/20220628213817_telegram_chat_title/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "telegram_destinations" ADD COLUMN     "chat_title" TEXT,
-ADD COLUMN     "is_group_chat" BOOLEAN;
--- backend/src/alerts/prisma/migrations/20220712213812_triggered_alert_slug/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `triggered_alert_slug` to the `triggered_alerts` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "triggered_alerts" ADD COLUMN     "triggered_alert_slug" TEXT NOT NULL;
