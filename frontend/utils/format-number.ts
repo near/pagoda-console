d6c0b5f7d75ce943038e268454a3aa7eb7d82492
Caleb Jacob <thecalebjacob@gmail.com>
2022-07-26 14:07:30 -0600

    Fixing formatNumber() to account for long decimal numbers

--- frontend/utils/format-number.ts
+++ frontend/utils/format-number.ts
@@ -1,11 +1,21 @@
 // 1 => 1
 // 1234 => 1,234
 // 1234.5555 => 1,234.5555
+// .34 => 0.34
 
-export function formatNumber(number: string) {
+export function formatNumber(number: string | number) {
+  if (!number) return '';
+
   const pattern = /(-?\d+)(\d{3})/;
-  while (pattern.test(number)) {
-    number = number.toString().replace(pattern, '$1,$2');
+  const segments = number.toString().split('.');
+  let integer = segments[0];
+  const decimal = segments[1];
+
+  while (pattern.test(integer)) {
+    integer = integer.toString().replace(pattern, '$1,$2');
   }
-  return number;
+
+  if (decimal) return `${integer || '0'}.${decimal}`;
+
+  return integer;
 }
