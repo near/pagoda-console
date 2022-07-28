f5593ed7c666af5a38f21f8bdd18ef8415785219
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-07-28 15:10:44 +0000

    add big number test cases

--- frontend/utils/sanitize-number.spec.ts
+++ frontend/utils/sanitize-number.spec.ts
@@ -4,6 +4,7 @@ test.each([
   ['1,000', '1000'],
   ['-01000', '-1000'],
   ['01,200.5010', '1200.501'],
+  ['00000012300005000,0000000000000000.0000', '123000050000000000000000000'],
 ])('should sanitize numbers correctly', (input, expected) => {
   expect(sanitizeNumber(input)).toBe(expected);
 });
