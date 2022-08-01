78bece105715017224a560f5dfbacb9d1d05df81
Charles Garrett <charleslavong@gmail.com>
2022-08-01 14:56:34 +0200

    replace mixpanel with segment analytics

--- frontend/utils/analytics.ts
+++ frontend/utils/analytics.ts
@@ -1,4 +1,5 @@
-import mixpanel from 'mixpanel-browser';
+import Analytics from 'analytics-node';
+import { uniqueId } from 'lodash-es';
 
 import config from './config';
 
@@ -6,34 +7,60 @@ export interface Dict {
   [key: string]: any;
 }
 
+let segment: Analytics;
+let userId: string;
+const anonymousId = uniqueId();
+
 function init() {
-  // Enabling the debug mode flag is useful during implementation,
-  // but it's recommended you remove it for production
-  mixpanel.init(config.mixpanel.token, {
-    debug: config.mixpanel.debug,
+  //flushAt=1 is useful for testing events,
+  //TODO switch this to enable: false to prevent recording events from local development
+  const options = config.segment.debug ? { flushAt: 1 } : {};
+  console.log('intitializing segment w/ %s', options);
+  segment = new Analytics(config.segment.key, options);
+}
+
+function alias() {
+  segment.alias({
+    previousId: anonymousId,
+    userId,
   });
 }
 
-function identify(userId: string) {
-  mixpanel.identify(userId);
+function track(eventLabel: string, properties?: Dict) {
+  const id = userId ? { userId } : { anonymousId };
+  segment.track({
+    ...id,
+    event: eventLabel,
+    properties,
+  });
 }
 
-function alias(id: string) {
-  mixpanel.alias(id);
+function identify(id: string) {
+  userId = id;
+  segment.identify({
+    userId,
+  });
+}
+
+function pageView(name: string, properties?: Dict) {
+  segment.page({
+    userId,
+    anonymousId,
+    name,
+    properties,
+  });
 }
 
 function reset() {
-  mixpanel.reset();
-}
-
-function track(eventName: string, eventDetails?: Dict) {
-  mixpanel.track(eventName, eventDetails);
+  segment.flush();
+  track('Logout');
 }
 
 const fns = {
-  init,
-  identify,
   alias,
+  identify,
+  init,
+  pageView,
   reset,
   track,
 };
