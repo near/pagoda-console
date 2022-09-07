b0d8af13ba7b95e9da23d9d4644020946b6bbb9b
judith-Near <103452915+judith-Near@users.noreply.github.com>
2022-09-07 15:20:57 -0400

    feat: Enhanced API URL change by network (#384)
    * prop changes, rerenders heading, does not rerender elements api
    
    * set is mainnet false when net dropdown changes to testnet
    
    * render both mainnet and testnet elements components

--- frontend/modules/apis/components/EnhancedApi.tsx
+++ frontend/modules/apis/components/EnhancedApi.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 
+import { Box } from '@/components/lib/Box';
 import { Message } from '@/components/lib/Message';
 import { Spinner } from '@/components/lib/Spinner';
 import { Text } from '@/components/lib/Text';
+import { useSelectedProject } from '@/hooks/selected-project';
 
 import { globalStyles } from './styles';
 
@@ -18,6 +20,17 @@ declare global {
 const EnhancedAPI = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [displayMessage, setDisplayMessage] = useState(true);
+  const [isMainnet, setIsMainnet] = useState<boolean>(false);
+
+  const { environment } = useSelectedProject();
+
+  useEffect(() => {
+    if (environment?.net === 'MAINNET') {
+      setIsMainnet(true);
+    } else {
+      setIsMainnet(false);
+    }
+  }, [environment?.net]);
 
   useEffect(() => {
     const script = document.createElement('script');
@@ -46,11 +59,21 @@ const EnhancedAPI = () => {
       )}
       {!isLoading ? (
         <div className="enhanced-api">
-          <elements-api
-            apiDescriptionUrl="https://near-enhanced-api-server-testnet-uxqf.onrender.com/api/spec/v3.json"
-            router="hash"
-            layout="sidebar"
-          />
+          <Box css={{ display: !isMainnet ? 'none' : undefined }}>
+            <elements-api
+              apiDescriptionUrl={'https://near-mainnet.api.pagoda.co/api/spec/v3.json'}
+              router="hash"
+              layout="sidebar"
+            />
+          </Box>
+
+          <Box css={{ display: isMainnet ? 'none' : undefined }}>
+            <elements-api
+              apiDescriptionUrl={'https://near-testnet.api.pagoda.co/api/spec/v3.json'}
+              router="hash"
+              layout="sidebar"
+            />
+          </Box>
         </div>
       ) : (
         <Spinner center />
