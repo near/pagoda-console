8aca6bd0daef8df36f9700e66b6b477b99c268ad
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-09-08 09:31:10 -0600

    add tag to script (#212)

--- backend/scripts/docker_build_and_push.sh
+++ backend/scripts/docker_build_and_push.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ -z "$1" ]
+then
+    echo "usage: ./docker_build_and_push.sh <dev|latest>";
+    exit 1;
+fi;
+
 # Build and push the docker container
-docker build -t gcr.io/near-dev-platform/developer-console-api .
-docker push gcr.io/near-dev-platform/developer-console-api
+docker build -t gcr.io/near-dev-platform/developer-console-api:$1 .
+docker push gcr.io/near-dev-platform/developer-console-api:$1
