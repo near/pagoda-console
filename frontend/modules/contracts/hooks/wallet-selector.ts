1fc0fc517161f227351aeb0023ee00d6083365b8
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-09-09 14:13:40 -0600

    don't reload window if we are going to redirect to another env's contracts page

--- frontend/modules/contracts/hooks/wallet-selector.ts
+++ frontend/modules/contracts/hooks/wallet-selector.ts
@@ -13,17 +13,30 @@ import { distinctUntilChanged, map } from 'rxjs';
 
 import { openToast } from '@/components/lib/Toast';
 import { useSelectedProject } from '@/hooks/selected-project';
+import type { NetOption } from '@/utils/types';
 
 // Cache in module to ensure we don't re-init
 let selector: WalletSelector | null = null;
 let modal: WalletSelectorModal | null = null;
 
+//! *** IMPORTANT ***
+// Do not call useWalletSelector hook anywhere but ContractTransaction.tsx. This current implementation is highly
+// coupled to being called once in ContractTransaction.tsx and serious side-effects may occur if this is changed.
 export const useWalletSelector = (contractId: string | undefined) => {
   const [accounts, setAccounts] = useState<Array<AccountState>>([]);
   const { environment } = useSelectedProject();
+  const [prevNet, setPrevNet] = useState<NetOption | undefined>();
 
   const init = useCallback(async () => {
     if (!contractId || !environment) return null;
+
+    //* Bail early if the environment has changed.
+    //* There is some logic in Header.tsx to redirect to the /contracts page
+    //* if environment changes and that should take precedence over this flow's window reload.
+    if (prevNet && prevNet !== environment.net) {
+      return null;
+    }
+    setPrevNet(environment.net);
 
     //* This is a hack, this is forcing a reload to be able to refresh the modal component
     //* and wallet selector to allow switching contract.
@@ -72,7 +85,7 @@ export const useWalletSelector = (contractId: string | undefined) => {
       });
 
     return () => subscription.unsubscribe();
-  }, [contractId, environment]);
+  }, [contractId, environment, prevNet]);
 
   useEffect(() => {
     init().catch((err) => {
