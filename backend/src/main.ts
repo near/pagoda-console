5bba9d787de66b0d254bac17309c671dbf35fb26
Alexey Immoreev <lexx92@mail.ru>
2022-09-24 01:19:27 +0400

    Root database url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,6 @@
+import { config } from 'dotenv';
+config();
+
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
