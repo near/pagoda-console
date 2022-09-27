2200f81501465b53e9de0d2728767d810474f3b1
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-09-27 08:03:17 -0600

    update export script

--- backend/scripts/export_prisma_env_vars.sh
+++ backend/scripts/export_prisma_env_vars.sh
@@ -5,17 +5,20 @@
 #* need to prefix the script command with a `.` or the word `source`
 #* in order to "source" the variables.
 #*
-#* e.g. `. ./prisma_env_vars.sh ...` or `source ./prisma_env_vars.sh ...`
+#* e.g. `. ./export_prisma_env_vars.sh ...` or `source ./export_prisma_env_vars.sh ...`
+
+echo "I was given $# argument(s):"
+printf "%s\n" "$@"
 
 if [ -z "$3" ]
 then
-    echo "usage: source ./prisma_env_vars.sh <user> <password> <url> <optional: db_connection_suffix>";
+    echo "usage: source ./export_prisma_env_vars.sh <user> <password> <url>";
     exit 1;
 fi;
 
 # TODO update new module script to append to this file
 # Sets env vars to connect to each Prisma database.
-export DATABASE_URL=postgresql://$1:$2@$3/devconsole$4
-export ALERTS_DATABASE_URL=postgresql://$1:$2@$3/alerts$4
-export ABI_DATABASE_URL=postgresql://$1:$2@$3/abi$4
-export RPCSTATS_DATABASE_URL=postgresql://$1:$2@$3/rpcstats$4
+export DATABASE_URL=postgresql://$1:$2@$3/devconsole
+export ALERTS_DATABASE_URL=postgresql://$1:$2@$3/alerts
+export ABI_DATABASE_URL=postgresql://$1:$2@$3/abi
+export RPCSTATS_DATABASE_URL=postgresql://$1:$2@$3/rpcstats
