1d94fea9226584cfb888dabbe985209b84c59f0c
Caleb Jacob <thecalebjacob@gmail.com>
2022-09-29 16:08:22 -0600

    Using SWR to fetch finality status to avoid duplicate useEffect() fetches

--- frontend/modules/contracts/components/ContractDetails.tsx
+++ frontend/modules/contracts/components/ContractDetails.tsx
@@ -1,6 +1,3 @@
-import JSBI from 'jsbi';
-import { useEffect, useState } from 'react';
-
 import TransactionAction from '@/components/explorer/transactions/TransactionAction';
 import { NetContext } from '@/components/explorer/utils/NetContext';
 import { Box } from '@/components/lib/Box';
@@ -11,14 +8,12 @@ import { Placeholder } from '@/components/lib/Placeholder';
 import { Spinner } from '@/components/lib/Spinner';
 import { Text } from '@/components/lib/Text';
 import { useContractMetrics } from '@/hooks/contracts';
-import config from '@/utils/config';
 import { convertYoctoToNear } from '@/utils/convert-near';
 import { formatBytes } from '@/utils/format-bytes';
 import type { Environment, NetOption } from '@/utils/types';
-import type { FinalityStatus } from '@/utils/types';
 import type { Contract } from '@/utils/types';
 
-import { useRecentTransactions } from '../hooks/recent-transactions';
+import { useFinalityStatus, useRecentTransactions } from '../hooks/recent-transactions';
 interface Props {
   contract?: Contract;
   environment?: Environment;
@@ -69,39 +64,8 @@ export function ContractDetails({ contract, environment }: Props) {
 function RecentTransactionList({ contract, net }: { contract?: Contract; net?: NetOption }) {
   // NOTE: This component and following code is legacy and will soon be replaced by new explorer components.
 
-  const [finalityStatus, setFinalityStatus] = useState<FinalityStatus>();
+  const { finalityStatus } = useFinalityStatus(net);
   const { transactions } = useRecentTransactions(contract?.address, net);
-
-  useEffect(() => {
-    if (!net) return;
-    fetchFinality(net);
-  }, [net]);
-
-  async function fetchFinality(net: NetOption) {
-    const res = await fetch(config.url.rpc.default[net], {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        jsonrpc: '2.0',
-        id: 'dontcare',
-        method: 'block',
-        params: {
-          finality: 'final',
-        },
-      }),
-    }).then((res) => res.json());
-    if (res.error) {
-      throw new Error(res.error.name);
-    }
-    const finalBlock = res.result;
-    const newStatus = {
-      finalBlockTimestampNanosecond: JSBI.BigInt(finalBlock.header.timestamp_nanosec),
-      finalBlockHeight: finalBlock.header.height,
-    };
-    setFinalityStatus(newStatus);
-  }
 
   return (
     <Flex stack>
