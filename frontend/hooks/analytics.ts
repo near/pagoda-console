c5aede9390fe0876d87e5a911eb02e8266b6a637
Caleb Jacob <thecalebjacob@gmail.com>
2022-10-21 15:32:42 -0600

    Improved page tracking by waiting for identify() to resolve

--- frontend/hooks/analytics.ts
+++ frontend/hooks/analytics.ts
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import analytics from '@/utils/analytics';
 
@@ -9,7 +9,22 @@ let lastTrackedPage = '';
 
 export function useAnalytics() {
   const router = useRouter();
-  const { user } = useAccount();
+  const { authenticationStatus, user } = useAccount();
+  const [hasIdentified, setHasIdentified] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      // https://segment.com/docs/connections/spec/best-practices-identify/
+
+      analytics.identify(user.uid, {
+        email: user.email,
+        displayName: user.name,
+        userId: user.uid,
+      });
+
+      setHasIdentified(true);
+    }
+  }, [user]);
 
   useEffect(() => {
     let page;
@@ -21,24 +36,17 @@ export function useAnalytics() {
 
     page = page.toUpperCase();
 
-    if (page === lastTrackedPage) return;
+    if (
+      page === lastTrackedPage ||
+      authenticationStatus === 'LOADING' ||
+      (authenticationStatus === 'AUTHENTICATED' && !hasIdentified)
+    )
+      return;
 
     lastTrackedPage = page;
     analytics.setAnonymousId();
     analytics.pageView(`DC View ${page} Page`, {
       path: router.pathname,
     });
-  }, [router.pathname]);
-
-  useEffect(() => {
-    if (user) {
-      // https://segment.com/docs/connections/spec/best-practices-identify/
-
-      analytics.identify(user.uid, {
-        email: user.email,
-        displayName: user.name,
-        userId: user.uid,
-      });
-    }
-  }, [user]);
+  }, [hasIdentified, authenticationStatus, router.pathname]);
 }
