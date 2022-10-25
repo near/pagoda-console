82f8656e7e695cc5a3123039c2990018f1934ad7
Caleb Jacob <thecalebjacob@gmail.com>
2022-10-25 17:25:53 -0600

    Adding header sign in link, updating project selector

--- frontend/components/layouts/DashboardLayout/ProjectSelector/ProjectSelector.tsx
+++ frontend/components/layouts/DashboardLayout/ProjectSelector/ProjectSelector.tsx
@@ -50,42 +50,46 @@ export function ProjectSelector(props: Props) {
       </DropdownMenu.Button>
 
       <DropdownMenu.Content width="trigger" innerCss={{ padding: 0, borderRadius: 'inherit' }}>
-        <Flex
-          css={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'stretch',
-            gap: 'var(--space-m)',
-            padding: 'var(--space-s)',
-          }}
-        >
-          {projectGroups?.map(([orgName, projects]) => {
-            return (
-              <div key={orgName}>
-                <DropdownMenu.ContentItem css={{ paddingBottom: 0 }}>
-                  <Text size="bodySmall" color="text3" css={{ textTransform: 'uppercase' }}>
-                    {orgName}
-                  </Text>
-                </DropdownMenu.ContentItem>
-                {projects.map((project) => (
-                  <DropdownMenu.Item key={project.id} onSelect={() => onSelectProject(project)}>
-                    {project.name}
-                    {project.tutorial && <Badge size="s">Tutorial</Badge>}
-                  </DropdownMenu.Item>
-                ))}
-              </div>
-            );
-          })}
-        </Flex>
+        {projectGroups && projectGroups.length > 0 && (
+          <Flex
+            css={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'stretch',
+              gap: 'var(--space-m)',
+              padding: 'var(--space-s)',
+            }}
+          >
+            {projectGroups?.map(([orgName, projects]) => {
+              return (
+                <div key={orgName}>
+                  <DropdownMenu.ContentItem css={{ paddingBottom: 0 }}>
+                    <Text size="bodySmall" color="text3" css={{ textTransform: 'uppercase' }}>
+                      {orgName}
+                    </Text>
+                  </DropdownMenu.ContentItem>
+                  {projects.map((project) => (
+                    <DropdownMenu.Item key={project.id} onSelect={() => onSelectProject(project)}>
+                      {project.name}
+                      {project.tutorial && <Badge size="s">Tutorial</Badge>}
+                    </DropdownMenu.Item>
+                  ))}
+                </div>
+              );
+            })}
+          </Flex>
+        )}
 
         <Flex
           css={{
             position: 'sticky',
             bottom: 0,
             background: 'var(--background-color)',
-            borderTop: 'solid 1px var(--color-border-1)',
-            marginTop: 'var(--space-s)',
             padding: 'var(--space-s)',
+
+            '&:not(:first-child)': {
+              borderTop: 'solid 1px var(--color-border-1)',
+            },
           }}
         >
           <DropdownMenu.Item
