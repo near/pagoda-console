67b5e900e075c6da3264000c3313093f8e90cd4a
milenakrawczyk <66628214+milenakrawczyk@users.noreply.github.com>
2022-10-26 22:18:49 +0200

    CI/CD pipeline - preview deployments (#103)
    * automate preview deployments
    
    * remove todo
    
    * Secure terraform state file
    
    * Sensitive db instance name
    
    * Making generate_temp_db_password.sh executable
    
    * Updating secret if it exists draft
    
    * Updating db secret
    
    * Fix docker image build
    
    * Remove generated dir
    
    * Remove npm ci install
    
    * Refactor Dockerfile
    
    * Debugging exporting env vars
    
    * Updating prisma command
    
    * Fix prisma reset command
    
    * Stub
    
    * Add frontend
    
    * Increase Docker build timeout
    
    * Builiding prisma
    
    * Michael's dockerfile rework
    
    * Prisma reset from db workspace
    
    * Installing deps before migrating
    
    * Update DB passwords
    
    * Update new secret script
    
    * Update DB pass for instances already created
    
    * Debugging connection problems
    
    * Restart api fix
    
    * Original solution cleanup
    
    * Fix vercel deployment name
    
    * API_BASE_URL fix
    
    * Fixing destroying preview deployments
    
    * Additional cleanup
    
    * Cleanup
    
    * trigger backend preview deploy on db dir change
    
    * add firebase client config to terraform variables
    
    * fix firebase client config variable definition for terraform
    
    * additional firebase client config declarations
    
    Co-authored-by: Jon Lewis <jonathandavidlewis92@gmail.com>
    Co-authored-by: Michael Peter <m.peter.dev@gmail.com>

---
+++ backend/scripts/vercel_upsert_env_var.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# Creates or updates an environment variable for a specific branch.
+
+if [ -z "$VERCEL_TEAM_ID" ]
+then
+    echo "Missing VERCEL_TEAM_ID variable";
+    exit 1;
+fi;
+
+if [ -z "$VERCEL_AUTH_TOKEN" ]
+then
+    echo "Missing VERCEL_AUTH_TOKEN variable";
+    exit 1;
+fi;
+
+if [ -z "$VERCEL_PROJECT_NAME" ]
+then
+    echo "Missing VERCEL_PROJECT_NAME variable";
+    exit 1;
+fi;
+
+if [ -z "$BRANCH_NAME" ]
+then
+    echo "Missing BRANCH_NAME variable";
+    exit 1;
+fi;
+
+if [ -z "$2" ]
+then
+    echo "usage: ./vercel_upsert_env_var.sh <name> <value>";
+    exit 1;
+fi;
+
+VAR_NAME=$1
+VAR_VALUE=$2
+
+# For reference: https://vercel.com/docs/rest-api#endpoints/projects/create-one-or-more-environment-variables
+RESPONSE=$(curl -X POST "https://api.vercel.com/v9/projects/${VERCEL_PROJECT_NAME}/env?teamId=${VERCEL_TEAM_ID}" \
+    -H "Authorization: Bearer ${VERCEL_AUTH_TOKEN}" \
+    -H "Content-Type: application/json" \
+    -d "{\"key\":\"${VAR_NAME}\",\"value\":\"${VAR_VALUE}\",\"target\":[\"preview\"],\"type\":\"encrypted\",\"gitBranch\":\"${BRANCH_NAME}\"}")
+
+ERROR=$(echo "$RESPONSE" | jq .error)
+
+if [ -z "$ERROR" ]
+then
+    echo ''
+    echo 'created env var'
+    exit 0
+fi
+
+ERROR_CODE=$(echo "$ERROR" | jq .code -r)
+
+if [ "$ERROR_CODE" = "ENV_ALREADY_EXISTS" ]
+then
+    echo "env var already exists... attempting to update"
+
+    # Attempt to update the env var in case it already exists.
+    # Helpful for writing filters in jq: https://gist.github.com/ipbastola/2c955d8bf2e96f9b1077b15f995bdae3
+    ENV_ID=$(curl -X GET "https://api.vercel.com/v9/projects/${VERCEL_PROJECT_NAME}/env?gitBranch=${BRANCH_NAME}&teamId=${VERCEL_TEAM_ID}" \
+        -H "Authorization: Bearer ${VERCEL_AUTH_TOKEN}" \
+        | jq .envs \
+        | jq -c ".[] | select( .key == \"${VAR_NAME}\" )" \
+        | jq .id -r)
+
+    echo ENV_ID $ENV_ID
+
+    curl -X PATCH "https://api.vercel.com/v9/projects/${VERCEL_PROJECT_NAME}/env/${ENV_ID}?teamId=${VERCEL_TEAM_ID}" \
+        -H "Authorization: Bearer ${VERCEL_AUTH_TOKEN}" \
+        -H "Content-Type: application/json" \
+        -d "{\"key\":\"${VAR_NAME}\",\"value\":\"${VAR_VALUE}\",\"target\":[\"preview\"],\"type\":\"encrypted\",\"gitBranch\":\"${BRANCH_NAME}\"}"
+fi
