f4680a490994ad95c099af91fadc7bf1cc9237ad
Caleb Jacob <thecalebjacob@gmail.com>
2022-10-27 09:20:16 -0600

    Including support for GTM for NEAR Foundation (#113)

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,6 +1,8 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 import { getCssText } from '@/styles/stitches';
+import config from '@/utils/config';
 import { initializeTheme } from '@/utils/initialize-theme';
 
 class MyDocument extends Document {
@@ -15,7 +17,27 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" />
 
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+
+          {/*
+            https://nextjs.org/docs/messages/next-script-for-ga
+            
+            The following Google Tag Manager inclusion is for the NEAR Foundation.
+            Analytics for Pagoda are tracked through Segment.
+          */}
+
+          {config.googleTagManagerId && (
+            <Script id="gtm" strategy="afterInteractive">
+              {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${config.googleTagManagerId}');
+              `}
+            </Script>
+          )}
         </Head>
+
         <body>
           <script dangerouslySetInnerHTML={{ __html: initializeTheme }} />
           <script defer src="https://p5c5wl39l4g2.statuspage.io/embed/script.js"></script>
