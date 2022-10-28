def040d5707b21e3b636e126ac639cc6ee00cd40
Caleb Jacob <thecalebjacob@gmail.com>
2022-10-28 11:34:36 -0600

    Hide log out button in sidebar if guest user

--- frontend/components/layouts/DashboardLayout/Sidebar/Sidebar.tsx
+++ frontend/components/layouts/DashboardLayout/Sidebar/Sidebar.tsx
@@ -86,6 +86,7 @@ export function Sidebar({ children, ...props }: Props) {
   const [sidebarCollapseButtonHover, setSidebarCollapseButtonHover] = useState(false);
   const sidebarCollapseToggleLabel = sidebarCollapsed ? 'Expand Sidebar' : 'Collapse Sidebar';
   const signOut = useSignOut();
+  const { authStatus } = useAuth();
 
   useEffect(() => {
     if (sidebarCollapsed === undefined) {
@@ -166,12 +167,15 @@ export function Sidebar({ children, ...props }: Props) {
         </S.Nav>
 
         <S.Nav>
-          <S.NavItem>
-            <S.NavLink as="button" type="button" onClick={signOut}>
-              <FeatherIcon icon="log-out" />
-              <S.NavLinkLabel>Logout</S.NavLinkLabel>
-            </S.NavLink>
-          </S.NavItem>
+          {authStatus === 'AUTHENTICATED' && (
+            <S.NavItem>
+              <S.NavLink as="button" type="button" onClick={signOut}>
+                <FeatherIcon icon="log-out" />
+                <S.NavLinkLabel>Logout</S.NavLinkLabel>
+              </S.NavLink>
+            </S.NavItem>
+          )}
+
           <S.NavItem>
             <ThemeToggle collapsed={sidebarCollapsed && !sidebarHoverExpand} />
           </S.NavItem>
