9e40232fed92a259503942599fbd3673a2fcbb79
Caleb Jacob <thecalebjacob@gmail.com>
2022-11-01 11:44:41 -0600

    Adding global stableId click event tracker

--- frontend/hooks/analytics.ts
+++ frontend/hooks/analytics.ts
@@ -1,8 +1,9 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import analytics from '@/utils/analytics';
 
+import { useBrowserLayoutEffect } from './browser-layout-effect';
 import { useAccount } from './user';
 
 let lastTrackedPage = '';
@@ -41,4 +42,59 @@ export function useAnalytics() {
       });
     }
   }, [user]);
+
+  useGlobalClickTracker();
+}
+
+function useGlobalClickTracker() {
+  const hoveredElementRef = useRef<HTMLElement | null>();
+
+  /*
+    NOTE 1: Due to how the Radix Dropdown Menu Trigger makes it impossible to directly capture the
+    "click" event, we have to get creative with a "hoveredElementRef" fallback.
+
+    NOTE 2: Sometimes <button> elements might contain child elements (eg: <span>, <svg>), these
+    child elements emit the click event, which means we need to traverse up the "parentElement"
+    at least twice to make sure we correctly capture the "data-stable-id" attribute.
+  */
+
+  function clickHandler(event: MouseEvent) {
+    if (event.target instanceof Element) {
+      const element = [
+        event.target,
+        event.target.parentElement,
+        event.target.parentElement?.parentElement,
+        hoveredElementRef.current,
+        hoveredElementRef.current?.parentElement,
+        hoveredElementRef.current?.parentElement?.parentElement,
+      ].find((el) => el?.hasAttribute('data-stable-id'));
+
+      if (element) {
+        const componentType = element.tagName.toLowerCase() === 'a' ? 'Link' : 'Button';
+        const stableId = element.getAttribute('data-stable-id');
+
+        analytics.track(`DC ${componentType} Clicked`, {
+          stableId,
+        });
+      }
+    }
+  }
+
+  function mouseoverHandler(event: MouseEvent) {
+    if (event.target instanceof Element) {
+      hoveredElementRef.current = event.target as HTMLElement;
+    } else {
+      hoveredElementRef.current = null;
+    }
+  }
+
+  useBrowserLayoutEffect(() => {
+    window.addEventListener('click', clickHandler);
+    document.body.addEventListener('mouseover', mouseoverHandler);
+
+    return () => {
+      window.removeEventListener('click', clickHandler);
+      document.body.removeEventListener('mouseover', mouseoverHandler);
+    };
+  });
 }
