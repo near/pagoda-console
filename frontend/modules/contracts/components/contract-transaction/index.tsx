80be28f9e04ceb234ae019c9679787ffe8570872
marcinbodnar <me@marcinbodnar.com>
2022-11-14 19:20:53 +0100

    Refactor ContractTransaction component

--- frontend/modules/contracts/components/contract-transaction/index.tsx
+++ frontend/modules/contracts/components/contract-transaction/index.tsx
@@ -2,43 +2,18 @@ import type { Api } from '@pc/common/types/api';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
-import TransactionActions from '@/components/explorer/transaction/TransactionActions';
-import { NetContext } from '@/components/explorer/utils/NetContext';
 import { Box } from '@/components/lib/Box';
-import { Card } from '@/components/lib/Card';
-import { CodeBlock } from '@/components/lib/CodeBlock';
 import { Flex } from '@/components/lib/Flex';
-import { H3, H5 } from '@/components/lib/Heading';
-import { List, ListItem } from '@/components/lib/List';
-import { Message } from '@/components/lib/Message';
-import { SvgIcon } from '@/components/lib/SvgIcon';
-import { Text } from '@/components/lib/Text';
 import { useRouteParam } from '@/hooks/route';
-import { useSelectedProject } from '@/hooks/selected-project';
 import { useWalletSelector } from '@/modules/contracts/hooks/wallet-selector';
-import TxList from '@/public/contracts/images/TxList.svg';
 import { styled } from '@/styles/stitches';
 import analytics from '@/utils/analytics';
 
 import ContractTransactionForm from './ContractTransactionForm';
+import TxResult from './TxResult';
 
 type Contract = Api.Query.Output<'/projects/getContract'>;
 
-const TextItalic = styled(Text, {
-  fontStyle: 'italic',
-});
-const ResultTitle = styled(H5, {
-  userSelect: 'none',
-});
-const ListWrapper = styled(List, {
-  [`& ${ListItem}`]: {
-    color: 'var(--color-text-1)',
-
-    '&::marker': {
-      color: 'inherit',
-    },
-  },
-});
 const ContractParams = styled(Box, {
   width: '26.25rem',
   flexShrink: 0,
@@ -100,87 +75,9 @@ const ContractTransaction = ({ contract }: Props) => {
       </ContractParams>
 
       <Box css={{ width: '100%', minWidth: '0' }}>
-        <TxResultView result={txResult} error={txError} />
+        <TxResult result={txResult} error={txError} />
       </Box>
     </Flex>
-  );
-};
-
-const TxResultView = ({ result, error }: { result: any; error: any }) => {
-  if (!result && !error) {
-    return <SendTransactionBanner />;
-  }
-
-  if (error) {
-    return (
-      <Flex stack>
-        <ResultTitle>Result</ResultTitle>
-        <Message type="error" content={error.toString()} />
-      </Flex>
-    );
-  }
-
-  if (result?.hash) {
-    const hash = result?.hash as string;
-    return (
-      <Flex stack>
-        <ResultTitle>Result</ResultTitle>
-        <TxResultHash hash={hash} />
-      </Flex>
-    );
-  }
-
-  return (
-    <Flex stack>
-      <ResultTitle>Result</ResultTitle>
-      <TxResult result={result} />
-    </Flex>
-  );
-};
-
-const SendTransactionBanner = () => (
-  <Card padding="xl">
-    <Flex>
-      <Box>
-        <SvgIcon size="xl" color="success" icon={TxList} />
-      </Box>
-      <Flex stack gap="l">
-        <H3>Sending a Transaction</H3>
-        <ListWrapper as="ol">
-          <ListItem>Select Function</ListItem>
-          <ListItem>Input function parameters</ListItem>
-          <ListItem>Send transaction</ListItem>
-        </ListWrapper>
-        <TextItalic>The transaction execution and history will show up here</TextItalic>
-      </Flex>
-    </Flex>
-  </Card>
-);
-
-const TxResult = ({ result }: { result: any }) => (
-  <Card padding="l">
-    <Box>
-      <CodeBlock>{JSON.stringify(result, null, 2)}</CodeBlock>
-    </Box>
-  </Card>
-);
-
-const TxResultHash = ({ hash }: { hash: string }) => {
-  const { environment } = useSelectedProject();
-  const net = environment?.net;
-
-  if (!net) {
-    return <></>;
-  }
-
-  return (
-    <Card padding="l">
-      <Box>
-        <NetContext.Provider value={net}>
-          <TransactionActions transactionHash={hash} />
-        </NetContext.Provider>
-      </Box>
-    </Card>
   );
 };
 
