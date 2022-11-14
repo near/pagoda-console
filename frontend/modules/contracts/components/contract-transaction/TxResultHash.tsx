282a284b1f54497466076168811303b60bca42c7
marcinbodnar <me@marcinbodnar.com>
2022-11-14 18:26:41 +0000

    Refactor TxResultHash component

--- frontend/modules/contracts/components/contract-transaction/TxResultHash.tsx
+++ frontend/modules/contracts/components/contract-transaction/TxResultHash.tsx
@@ -13,24 +13,21 @@ const ResultTitle = styled(H5, {
 
 const TxResultHash = ({ result }: { result: any }) => {
   const hash = result?.hash as string;
-
   const { environment } = useSelectedProject();
   const net = environment?.net;
-
-  if (!net) {
-    return <></>;
-  }
 
   return (
     <Flex stack>
       <ResultTitle>Result</ResultTitle>
-      <Card padding="l">
-        <Box>
-          <NetContext.Provider value={net}>
-            <TransactionActions transactionHash={hash} />
-          </NetContext.Provider>
-        </Box>
-      </Card>
+      {net && (
+        <Card padding="l">
+          <Box>
+            <NetContext.Provider value={net}>
+              <TransactionActions transactionHash={hash} />
+            </NetContext.Provider>
+          </Box>
+        </Card>
+      )}
     </Flex>
   );
 };
