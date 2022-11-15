6790986a21c64e6b333cc150d7c91357c890600a
marcinbodnar <me@marcinbodnar.com>
2022-11-15 20:41:22 +0100

    Extract and create TxFormGas component

--- frontend/modules/contracts/components/contract-transaction/TxForm.tsx
+++ frontend/modules/contracts/components/contract-transaction/TxForm.tsx
@@ -10,14 +10,11 @@ import * as Form from '@/components/lib/Form';
 import { H5 } from '@/components/lib/Heading';
 import { NearInput } from '@/components/lib/NearInput';
 import { openToast } from '@/components/lib/Toast';
-import { Tooltip } from '@/components/lib/Tooltip';
 import { initContractMethods, useAnyAbi } from '@/modules/contracts/hooks/abi';
 import { useWalletSelector } from '@/modules/contracts/hooks/wallet-selector';
 import * as gasUtils from '@/modules/contracts/utils/convert-gas';
 import { styled } from '@/styles/stitches';
 import analytics from '@/utils/analytics';
-import { numberInputHandler } from '@/utils/input-handlers';
-import { sanitizeNumber } from '@/utils/sanitize-number';
 import { StableId } from '@/utils/stable-ids';
 import type { Contract } from '@/utils/types';
 import { validateInteger, validateMaxNearU128, validateMaxYoctoU128 } from '@/utils/validations';
@@ -25,34 +22,11 @@ import { validateInteger, validateMaxNearU128, validateMaxYoctoU128 } from '@/ut
 import convertNearDeposit from '../utils/convertNearDeposit';
 import resolveAbiDefinition from '../utils/resolveAbiDefinition';
 import TxFormSelectFunction from './TxFormFunction';
+import TxFormGas from './TxFormGas';
 import TxFormWalletLogin from './TxFormWalletLogin';
 
 const SectionTitle = styled(H5, {
   userSelect: 'none',
-});
-
-const UseMaxButton = styled(Button, {
-  textTransform: 'uppercase',
-  position: 'absolute',
-  right: 0,
-  top: 0,
-  color: 'var(--color-primary) !important',
-  fontSize: 'var(--font-size-body-small) !important',
-
-  '&:hover': {
-    background: 'transparent !important',
-  },
-  '&:focus': {
-    outline: 'none',
-  },
-
-  variants: {
-    hidden: {
-      true: {
-        visibility: 'hidden',
-      },
-    },
-  },
 });
 
 interface ContractFormProps {
@@ -254,45 +228,7 @@ const TxForm = ({ contract, onTxResult, onTxError }: ContractFormProps) => {
             </Flex>
 
             <Flex inline>
-              <Form.Group>
-                <Tooltip content="On NEAR, all unused gas will be refunded after the transaction.">
-                  <Form.FloatingLabelInput
-                    type="string"
-                    label="Gas:"
-                    isInvalid={!!form.formState.errors.gas}
-                    onInput={(event) => {
-                      numberInputHandler(event, { allowComma: false, allowDecimal: false, allowNegative: false });
-                    }}
-                    {...form.register(`gas`, {
-                      setValueAs: (value) => sanitizeNumber(value),
-                      validate: {
-                        minValue: (value: string) =>
-                          JSBI.greaterThan(JSBI.BigInt(value), JSBI.BigInt(0)) ||
-                          'Value must be greater than 0. Try using 10 Tgas',
-                        maxValue: (value: string) =>
-                          JSBI.lessThan(
-                            gasUtils.convertGasByFormat(value, gasFormat),
-                            JSBI.BigInt(gasUtils.convertGasToTgas('301')),
-                          ) || 'You can attach a maximum of 300 Tgas to a transaction',
-                      },
-                    })}
-                  />
-                </Tooltip>
-
-                <Form.Feedback>{form.formState.errors.gas?.message}</Form.Feedback>
-
-                <UseMaxButton
-                  stableId={StableId.CONTRACT_TRANSACTION_MAX_GAS_BUTTON}
-                  color="transparent"
-                  onClick={() => {
-                    form.setValue('gas', '300');
-                    form.setValue('gasFormat', 'Tgas');
-                  }}
-                  hidden={Boolean(gas)}
-                >
-                  Use Max
-                </UseMaxButton>
-              </Form.Group>
+              <TxFormGas form={form} gasFormat={gasFormat} gas={gas} />
 
               <DropdownMenu.Root>
                 <DropdownMenu.Button
