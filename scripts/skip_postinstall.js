a2f2b5b1cf837b66f1eb92143abfeb1f8f33afe4
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-11-16 08:42:55 -0700

    remove script

--- scripts/skip_postinstall.js
+++
@@ -1,7 +0,0 @@
-// We have a postinstall script to build the database and eslint packages. We don't need these packages to be built in Vercel's CI when building the frontend and this script allows us to skip this step.
-// Vercel environment variables: https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables
-if (process.env.VERCEL === "1" && process.env.CI === "1") {
-  process.exit(0);
-} else {
-  process.exit(1);
-}
