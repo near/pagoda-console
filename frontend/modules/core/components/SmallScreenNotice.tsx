4a0a7ac2cd01f5194264c777c0b2369d09966515
Caleb Jacob <thecalebjacob@gmail.com>
2022-12-01 14:59:41 -0700

    Small screen notice updates

--- frontend/modules/core/components/SmallScreenNotice.tsx
+++ frontend/modules/core/components/SmallScreenNotice.tsx
@@ -5,6 +5,7 @@ import { Box } from '@/components/lib/Box';
 import { Container } from '@/components/lib/Container';
 import { Flex } from '@/components/lib/Flex';
 import { H1 } from '@/components/lib/Heading';
+import { Section } from '@/components/lib/Section';
 import { Text } from '@/components/lib/Text';
 import Logo from '@/public/images/brand/pagoda-logo.svg';
 import Circles from '@/public/images/circles.svg';
@@ -20,32 +21,6 @@ export default function SmallScreenNotice({ children }: Props) {
 
   return (
     <>
-      <Flex
-        align="center"
-        justify="center"
-        css={{
-          display: 'none',
-          margin: 'auto',
-
-          '@tablet': {
-            display: 'flex',
-          },
-        }}
-      >
-        <Circles style={{ position: 'absolute', top: 0, right: 0 }} />
-
-        <Container size="s">
-          <Flex stack gap="l">
-            <H1>See you on the big screen!</H1>
-            <Text>
-              We’re working to deliver the best mobile experience possible. In the meantime, please visit this page on a
-              device with a larger screen.
-            </Text>
-            <Logo style={{ height: '40px' }} />
-          </Flex>
-        </Container>
-      </Flex>
-
       <Box
         css={{
           '@tablet': {
@@ -55,6 +30,32 @@ export default function SmallScreenNotice({ children }: Props) {
       >
         {children}
       </Box>
+
+      <Section css={{ margin: 'auto' }}>
+        <Flex
+          align="center"
+          justify="center"
+          css={{
+            display: 'none',
+            '@tablet': {
+              display: 'flex',
+            },
+          }}
+        >
+          <Circles style={{ position: 'absolute', top: 'var(--size-header-height)', right: 0 }} />
+
+          <Container size="s">
+            <Flex stack gap="l">
+              <H1>See you on the big screen!</H1>
+              <Text>
+                We’re working to deliver the best mobile experience possible. In the meantime, please visit this page on
+                a device with a larger screen.
+              </Text>
+              <Logo style={{ height: '40px' }} />
+            </Flex>
+          </Container>
+        </Flex>
+      </Section>
     </>
   );
 }
