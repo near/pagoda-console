03ca270b37f202a6620aab8cc72fd5e90359488d
Michael Peter <m.peter.dev@gmail.com>
2022-12-02 20:24:28 +0000

    docker and postgres setup for strapi

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -1,16 +1,38 @@
 import path from "path";
 
-export default ({ env }) => ({
-  connection: {
-    client: "sqlite",
-    connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        "..",
-        env("DATABASE_FILENAME", ".data/data.db")
-      ),
-    },
-    useNullAsDefault: true,
-  },
-});
+export default ({ env }) => {
+  // use SQLite when running locally
+  if (env("CMS_DB_TYPE") === "SQLITE") {
+    return {
+      connection: {
+        client: "sqlite",
+        connection: {
+          filename: path.join(
+            __dirname,
+            "..",
+            "..",
+            env("DATABASE_FILENAME", ".data/data.db")
+          ),
+        },
+        useNullAsDefault: true,
+      },
+    };
+  } else if (env("CMS_DB_TYPE") === "POSTGRES") {
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: env("DATABASE_HOST"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: "cms", // hardcode this to CMS so we never accidentally conflict with other data
+          user: env("DATABASE_USERNAME"),
+          password: env("DATABASE_PASSWORD"),
+          ssl: false,
+        },
+        debug: false,
+      },
+    };
+  } else {
+    throw new Error("Please specify CMS_DB_TYPE");
+  }
+};
