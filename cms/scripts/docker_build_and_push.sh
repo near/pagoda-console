de6b0d6f1d9ee923c92f92aa57ac7636ff16dc5d
Michael Peter <m.peter.dev@gmail.com>
2022-12-02 20:28:28 +0000

    add docker build and push script for cms

---
+++ cms/scripts/docker_build_and_push.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+    echo "usage: ./docker_build_and_push.sh <dev|latest|branch_name> <optional: gcp project id>";
+    exit 1;
+fi;
+
+# TODO use $PROJECT_ID 
+PROJECT_ID="$2"
+if [ -z "$2" ]
+then
+    PROJECT_ID="near-dev-platform"
+fi;
+
+TAG="gcr.io/$PROJECT_ID/pagoda-console-cms:$1"
+
+echo "Docker tag: $TAG"
+
+# Build and push the docker container
+docker build -t "$TAG" -f ./Dockerfile ../
+docker push "$TAG"
