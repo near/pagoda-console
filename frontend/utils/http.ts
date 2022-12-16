f6e3086f8c6306375ebc1ebe9db67138ab5c1152
Jon Lewis <jonathandavidlewis92@gmail.com>
2022-12-16 12:59:06 -0700

    fix http util

--- frontend/utils/http.ts
+++ frontend/utils/http.ts
@@ -20,6 +20,7 @@ export const fetchApi = async <K extends Api.Query.Key | Api.Mutation.Key>(
   let headers: HeadersInit = {
     'Content-Type': 'application/json',
   };
+
   if (!unauth) {
     const user = getAuth().currentUser;
     if (!user) {
@@ -27,26 +28,39 @@ export const fetchApi = async <K extends Api.Query.Key | Api.Mutation.Key>(
     }
     headers = { ...headers, Authorization: `Bearer ${await getIdToken(user)}` };
   }
+
   const response = await fetch(`${config.url.api}${endpoint}`, {
     method: 'POST',
     headers,
     body: input ? JSON.stringify(input) : undefined,
   });
 
+  let json;
+
   try {
-    const json = await response.json();
-    if (!response.ok) {
-      throw json;
+    if (response.status === 204) {
+      json = {};
+    } else {
+      json = await response.json();
     }
-    return json;
   } catch (e) {
     if (response.ok) {
+      // This will happen if a response is successful and has no body
       return undefined as unknown as K extends Api.Query.Key
         ? Api.Query.Output<K>
         : K extends Api.Mutation.Key
         ? Api.Mutation.Output<K>
         : never;
     }
-    throw new Error('Unknown JSON error');
   }
+
+  if (!response.ok) {
+    throw json;
+  }
+
+  return json as unknown as K extends Api.Query.Key
+    ? Api.Query.Output<K>
+    : K extends Api.Mutation.Key
+    ? Api.Mutation.Output<K>
+    : never;
 };
