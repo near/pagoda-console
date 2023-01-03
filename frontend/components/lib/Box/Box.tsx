02635269226e3be6ee8c1c249ebd276db4765ebf
marcinbodnar <me@marcinbodnar.com>
2023-01-03 17:31:12 +0000

    Add Box variants

--- frontend/components/lib/Box/Box.tsx
+++ frontend/components/lib/Box/Box.tsx
@@ -1,3 +1,14 @@
 import { styled } from '@/styles/stitches';
+import { background, padding } from '@/styles/variants';
 
-export const Box = styled('div', {});
+export const Box = styled('div', {
+  variants: {
+    padding,
+    background,
+  },
+
+  defaultVariants: {
+    padding: 'none',
+    background: 'none',
+  },
+});
