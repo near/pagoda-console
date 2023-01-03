9195213791e7566559101725d3552b4b949947ba
marcinbodnar <me@marcinbodnar.com>
2023-01-03 17:35:03 +0000

    update Box variants

--- frontend/components/lib/Box/Box.tsx
+++ frontend/components/lib/Box/Box.tsx
@@ -1,10 +1,41 @@
 import { styled } from '@/styles/stitches';
-import { background, padding } from '@/styles/variants';
 
 export const Box = styled('div', {
   variants: {
-    padding,
-    background,
+    padding: {
+      none: {
+        padding: '0rem',
+      },
+      s: {
+        padding: 'var(--space-s)',
+      },
+      m: {
+        padding: 'var(--space-m)',
+      },
+      l: {
+        padding: 'var(--space-l)',
+      },
+      xl: {
+        padding: 'var(--space-xl)',
+      },
+    },
+    background: {
+      none: {
+        background: '',
+      },
+      surface1: {
+        background: 'var(--color-surface-1)',
+      },
+      surface2: {
+        background: 'var(--color-surface-2)',
+      },
+      surface3: {
+        background: 'var(--color-surface-3)',
+      },
+      surface4: {
+        background: 'var(--color-surface-4)',
+      },
+    },
   },
 
   defaultVariants: {
