092d4fa4455192d2849334f519e70ab54cf10831
marcinbodnar <me@marcinbodnar.com>
2023-01-11 23:36:20 +0100

    Update HeaderText props

--- frontend/modules/deploys/components/header/HeaderText.tsx
+++ frontend/modules/deploys/components/header/HeaderText.tsx
@@ -2,9 +2,9 @@ import { FeatherIcon } from '@/components/lib/FeatherIcon';
 import { Flex } from '@/components/lib/Flex';
 import { Text } from '@/components/lib/Text';
 
-const HeaderText = ({ children }) => (
+const HeaderText = ({ children, ...rest }) => (
   <Flex align="center">
-    <FeatherIcon size="m" icon="" />
+    <FeatherIcon size="m" icon="" {...rest} />
     <Text size="h5" family="number" color="text1">
       {children}
     </Text>
