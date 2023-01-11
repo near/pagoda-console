885de82b52ed7f7c2c0b79516e7e3c1114452907
marcinbodnar <me@marcinbodnar.com>
2023-01-11 23:39:38 +0100

    Add rows to History component

--- frontend/modules/deploys/components/history/index.tsx
+++ frontend/modules/deploys/components/history/index.tsx
@@ -1,11 +1,15 @@
+import { Badge } from '@/components/lib/Badge';
 import { Button } from '@/components/lib/Button';
 import { FeatherIcon } from '@/components/lib/FeatherIcon';
 import { Flex } from '@/components/lib/Flex';
 import { H5 } from '@/components/lib/Heading';
 import * as Table from '@/components/lib/Table';
+import { Text } from '@/components/lib/Text';
 import { StableId } from '@/utils/stable-ids';
 
 const History = () => {
+  const history = [1, 2, 3, 4];
+
   return (
     <Table.Root>
       <Table.Head>
@@ -37,6 +41,52 @@ const History = () => {
           <Table.HeaderCell></Table.HeaderCell>
         </Table.Row>
       </Table.Head>
+
+      <Table.Body>
+        {history.map((i) => (
+          <Table.Row key={i}>
+            <Table.Cell>
+              <Flex gap="xs">
+                <Text size="bodySmall" color="text1">
+                  dev
+                </Text>
+                <Text size="bodySmall" color="text3">
+                  @
+                </Text>
+                <Text size="bodySmall" color="primary" family="number">
+                  4166bf9
+                </Text>
+              </Flex>
+            </Table.Cell>
+            <Table.Cell>
+              <Text size="bodySmall" family="number" color="text1">
+                Today at 4:35 PM
+              </Text>
+            </Table.Cell>
+            <Table.Cell>
+              <Text size="bodySmall" family="number" color="text2">
+                <Badge size="s">
+                  <FeatherIcon size="xs" icon="code" color="warning" />
+                  Testnet
+                </Badge>{' '}
+                1m 58s
+              </Text>
+            </Table.Cell>
+            <Table.Cell>
+              <Text size="bodySmall" color="text1">
+                Fixed deployment flow
+              </Text>
+            </Table.Cell>
+            <Table.Cell>
+              <Flex justify="end">
+                <Button stableId={StableId.DEPLOYS_HISTORY_RECORD} hideText="tablet" size="s" color="neutral">
+                  <FeatherIcon size="xs" icon="zap" />
+                </Button>
+              </Flex>
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
     </Table.Root>
   );
 };
