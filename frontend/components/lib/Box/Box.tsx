f754eff3c3c005f355bc78c91e0ba0923e73d58e
marcinbodnar <me@marcinbodnar.com>
2023-01-11 23:28:07 +0100

    Update Box component styling

--- frontend/components/lib/Box/Box.tsx
+++ frontend/components/lib/Box/Box.tsx
@@ -1,3 +1,27 @@
 import { styled } from '@/styles/stitches';
 
-export const Box = styled('div', {});
+export const Box = styled('div', {
+  variants: {
+    padding: {
+      none: {
+        padding: '0rem',
+      },
+      s: {
+        padding: 'var(--space-s)',
+      },
+      m: {
+        padding: 'var(--space-m)',
+      },
+      l: {
+        padding: 'var(--space-l)',
+      },
+      xl: {
+        padding: 'var(--space-xl)',
+      },
+    },
+  },
+
+  defaultVariants: {
+    padding: 'none',
+  },
+});
