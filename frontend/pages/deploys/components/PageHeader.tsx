29135402fe2054b093796404ea18508b84010b9c
marcinbodnar <me@marcinbodnar.com>
2023-01-11 23:37:43 +0100

    Add buttons to PageHeader

--- frontend/pages/deploys/components/PageHeader.tsx
+++ frontend/pages/deploys/components/PageHeader.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/lib/Button';
 import { FeatherIcon } from '@/components/lib/FeatherIcon';
 import { Flex } from '@/components/lib/Flex';
 import { H1 } from '@/components/lib/Heading';
 import { Section } from '@/components/lib/Section';
+import { StableId } from '@/utils/stable-ids';
 
 const PageHeader = () => (
   <Section>
@@ -12,6 +14,15 @@ const PageHeader = () => (
           <H1>Deploys</H1>
         </Flex>
 
+        <Flex justify="end">
+          <Button color="neutral" stableId={StableId.DEPLOYS_GITHUB_REPO} hideText="tablet">
+            <FeatherIcon icon="share" /> GitHub Repo
+          </Button>
+
+          <Button stableId={StableId.DEPLOYS_CODE} hideText="tablet">
+            <FeatherIcon icon="code" /> Code
+          </Button>
+        </Flex>
       </Flex>
     </Flex>
   </Section>
