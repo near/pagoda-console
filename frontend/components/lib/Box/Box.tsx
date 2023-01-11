eae4c39e8a3dcb4d4f7836fcbb826710b1f0f00a
marcinbodnar <me@marcinbodnar.com>
2023-01-11 23:28:20 +0100

    Additional Box styling

--- frontend/components/lib/Box/Box.tsx
+++ frontend/components/lib/Box/Box.tsx
@@ -19,9 +19,27 @@ export const Box = styled('div', {
         padding: 'var(--space-xl)',
       },
     },
+    background: {
+      none: {
+        background: '',
+      },
+      surface1: {
+        background: 'var(--color-surface-1)',
+      },
+      surface2: {
+        background: 'var(--color-surface-2)',
+      },
+      surface3: {
+        background: 'var(--color-surface-3)',
+      },
+      surface4: {
+        background: 'var(--color-surface-4)',
+      },
+    },
   },
 
   defaultVariants: {
     padding: 'none',
+    background: 'none',
   },
 });
