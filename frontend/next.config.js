49711c0ce5fae30a69c380b08fde3e8b344afa62
Jon Lewis <jonathandavidlewis92@gmail.com>
2023-02-16 17:18:27 -0700

    add frame-ancestors none

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,6 +14,10 @@ const moduleExports = {
             key: 'X-Frame-Options',
             value: 'DENY',
           },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'none'",
+          },
         ],
       },
     ];
