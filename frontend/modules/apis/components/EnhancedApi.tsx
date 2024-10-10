f3db3a95d76cbe3ce3eb4a455755ff436b9ae275
Darun Seethammagari <darunrs@gmail.com>
2024-10-09 19:07:39 -0700

    Update message to final version and remove previous text

--- frontend/modules/apis/components/EnhancedApi.tsx
+++ frontend/modules/apis/components/EnhancedApi.tsx
@@ -29,9 +29,8 @@ const EnhancedAPI = () => {
     <Flex stack>
       {
         <Message type="error">
-          <Text>Want to interact with RPC API? Check out the key tab for the setup guide.</Text>
           <Text>
-            Enhanced API is being decomissioned by Dec 9, 2024. Please cease usage by then to avoid disruptions.
+            Enhanced API is being decomissioned by Dec 9, 2024. Please move to another solution before that date.
           </Text>
         </Message>
       }
